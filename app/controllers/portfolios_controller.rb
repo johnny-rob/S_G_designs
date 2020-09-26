1129ca3172affb40f24333b70f22ee8b1369600c
johnny-rob <jpinney1@gmail.com>
2020-09-26 15:42:17 -0400

    Implemented friendly_id to the blog and the portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -64,7 +64,7 @@ class PortfoliosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_portfolio
-      @portfolio = Portfolio.find(params[:id])
+      @portfolio = Portfolio.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
