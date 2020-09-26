bbafeb0a8433ba7f9c5886a5c7544b7ee5b79650
johnny-rob <jpinney1@gmail.com>
2020-09-26 12:18:52 -0400

    Implemented pages and navigation, created shared partial for navigation menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'pages#interiors'
+  get 'about' => 'pages#about'
+  get 'services' => 'pages#services'
+  get 'contact', to:'pages#contact'
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
 
 end
